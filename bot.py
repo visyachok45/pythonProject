063dbf2eb6e77b746ec271e3c6d5569cadb3ea2a
visyachok45 <bochkovadiana@yandex.ru>
2022-06-01 16:36:49 +0300

    +сон4

--- bot.py
+++ bot.py
@@ -342,6 +342,24 @@ async def snow(message: types.Message):
     users[str(user_id)]["state"] = "INPUT_DAY_DESCR"
 
 
+@dp.message_handler(filters.Text(contains="Ветренняя"))
+async def windy(message: types.Message):
+    user_id = message.from_user.id
+    user = users.get(str(user_id))
+    if user == None:
+        state = "FINISH"
+    else:
+        state = user['state']
+    if state != "INPUT_WEATHER":
+        await message.reply("Неверный ввод")
+        return
+    diary[str(user_id)][user["date"]]["Погода - "] = message.text
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*sleep)
+    await message.reply("Бррр! Небось холодновато. Какая прошел твой день?", reply_markup=keyboard)
+    users[str(user_id)]["state"] = "INPUT_DAY_DESCR"
+
+
 @dp.message_handler(filters.Text(contains="весь"))
 async def print_diary(message: types.Message):
     user_id = message.from_user.id
